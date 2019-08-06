b8523d1270baf578969a9779b416bf29fb7b3be9
Dalan Ienatsch <dienatsch93@gmail.com>
2019-08-06 00:51:14 -0400

    Updates

---
+++ precache-manifest.05b866aad8b1fec0e8efb49303f84d57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7d0c605d0b11842c6dcd9a5907969fb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5c5349c7bd5704f8d67b",
+    "url": "/Portfolio/static/css/main.882a584a.chunk.css"
+  },
+  {
+    "revision": "8917f717f46f21e61516",
+    "url": "/Portfolio/static/js/2.a1a0516e.chunk.js"
+  },
+  {
+    "revision": "5c5349c7bd5704f8d67b",
+    "url": "/Portfolio/static/js/main.2f939bc8.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  }
+]);
