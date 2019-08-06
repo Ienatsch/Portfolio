5c60ed31a466bc3e149eef869956f3c9c811270f
Dalan Ienatsch <dienatsch93@gmail.com>
2019-08-06 12:55:32 -0400

    Added projects and config file containing links

--- src/components/infoPallet/projectsPallet.jsx
+++ src/components/infoPallet/projectsPallet.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import "../infoPallet/pallet.css";
+import Config from "../../config.json";
+
 function ProjectsPallet() {
+
+    function routeToProject(link){
+        window.open(link, '_blank');
+    }
+
     return (
         <div className="pallet">
             <div className="projectsContainer">
-                <figure>
+                <figure onClick={() => routeToProject(Config.projectUrls.SEEKER)}>
                     <img src="" alt="Seeker-Project" />
                     <figcaption>SEEKER</figcaption>
                 </figure>
-                <figure>
-                    <img src="" alt="Seeker-Project" />
-                    <figcaption>SEEKER</figcaption>
+                <figure onClick={() => routeToProject(Config.projectUrls.NATPK)}>
+                    <img src="" alt="NATPK-Project" />
+                    <figcaption>NATPK</figcaption>
                 </figure>
             </div>
         </div>
